54f6c1f8f8ebba0592cce00354ad68ac577e95c6
ERieu <evan.rieu@gmail.com>
2024-03-29 11:40:17 +0100

    save 1

---
+++ src/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val eclairage = AmpouleLed();
+    /**
+     * Cycles d'éclairage et d'extinction d'une ampoule
+     */
+    var nbCycles = 0;
+
+    do {
+        eclairage.allumer();
+
+        while(eclairage.etat() > 0 ){
+            eclairage.diminuer();
+        }
+
+        println(eclairage.toString());
+        nbCycles ++;
+    }
+
+    while (nbCycles < 1000 && eclairage.etat() != -1);
+
+    println("nombre de cycles "+nbCycles);
+}
